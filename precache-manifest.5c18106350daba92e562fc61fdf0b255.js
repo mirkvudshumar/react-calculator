32d95292a86390e66d8dfdc33363d11c1178f69c
Your Name <nikolovskialexxandar@gmail.com>
2020-01-04 18:23:08 +0100

    Updates

---
+++ precache-manifest.5c18106350daba92e562fc61fdf0b255.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4adc6429546c130d771cc6efc4f3b5a0",
+    "url": "/react-calculator/index.html"
+  },
+  {
+    "revision": "31ce6993b164dd36b48f",
+    "url": "/react-calculator/static/css/main.508f4616.chunk.css"
+  },
+  {
+    "revision": "ec72e036676debebe235",
+    "url": "/react-calculator/static/js/2.4d42537d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-calculator/static/js/2.4d42537d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "31ce6993b164dd36b48f",
+    "url": "/react-calculator/static/js/main.85106d8a.chunk.js"
+  },
+  {
+    "revision": "cebd5e570a9dabb2694b",
+    "url": "/react-calculator/static/js/runtime-main.da1d863d.js"
+  }
+]);
