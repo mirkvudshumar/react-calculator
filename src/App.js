2212faaf6571dd0b130b846a5c11edc609a4f044
segasunset95 <nikolovskialexxandar@gmail.com>
2021-03-16 19:21:31 +0100

    Delete unnecessary components, make App.js main container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,74 @@
 import React, { Component } from 'react';
+
 import './App.css';
-import Calculator from './containers/Calculator/Calculator';
+
+import Button from './components/UI/Button/Button';
+import Result from './components/Result/Result'
 
 class App extends Component {
+  state = {
+    equationArr: [],
+  };
+
+  onButtonPress = (event) => {
+    let equation = this.state.equationArr;
+    switch (event.target.value) {
+      case 'AC':
+        equation = [];
+        break;
+      case '=':
+        if (Array.isArray(equation)) {
+          try {
+            equation = eval(this.state.equationArr.join(''));
+          } catch (error) {
+            alert('error');
+          } finally {
+            equation = eval(this.state.equationArr.join(''));
+          }
+        }
+        break;
+      case 'C':
+        if (Array.isArray(equation) && equation.length > 0) {
+          equation.pop();
+        }
+        break;
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+      case '.':
+        if (Array.isArray(equation)) {
+          equation.push(event.target.value);
+        }
+        break;
+    }
+    this.setState({ equationArr: equation });
+  };
+
   render() {
+    let keys = ['AC', 'C', 'M', '+', 9, 8, 7, '-', 6, 5, 4, '*', 3, 2, 1, '/', '.', 0, '='];
+    keys = keys.map((el) => (
+      <Button
+        key={el}
+        value={el}
+        clicked={(event) => this.onButtonPress(event)}
+      />
+    ));
+
     return (
-      <div className="App">
-        <Calculator />
+      <div className="calculator">
+        <Result equation={this.state.equationArr} />
+        <ul>{keys}</ul>
       </div>
     );
   }
