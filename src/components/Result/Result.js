1c88deb2b5efa9b0936e7e2ec9e00c07abf88e51
segasunset95 <nikolovskialexxandar@gmail.com>
2021-03-16 21:31:44 +0100

    Lint code, switch to standard CSS instead of modules
    - Modules are not necessary, swtich to standard CSS

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-import styles from './Result.module.css'
-const result = (props) => {
-		return (
-			<form>
-				<input type="text" value={Array.isArray(props.equation) ? props.equation.join('') : props.equation}>
-				</input>
-			</form>
-		);
-}
+import './Result.css';
+
+const result = (props) => (
+  <form>
+    <input
+      type="text"
+      value={
+        Array.isArray(props.equation) ? props.equation.join('') : props.equation
+      }
+    />
+  </form>
+);
 
 export default result;
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,20 +1,31 @@
-import React from 'react';	
+import React from 'react';
 
-import styles from './Button.module.css';
+import './Button.css';
 
 const Button = (props) => {
-	let customStyle = [];
-	if(props.value === '+' || props.value === '-' || props.value === '*' || props.value === '/') {
-		customStyle.push(styles.buttonOperation)
-	}
-	else if (props.value === '=') {
-		customStyle.push(styles.buttonRed);
-	}
-	return (
-		<li style={props.value === '=' ? {'width': '44%'} : {width: "20%"} }>
-			<button className={customStyle.join('')} type="button" onClick={props.clicked} value={props.value}>{props.value}</button>
-		</li>
-	);
-}
+  let customStyle = [];
+  if (
+    props.value === '+' ||
+    props.value === '-' ||
+    props.value === '*' ||
+    props.value === '/'
+  ) {
+    customStyle.push('buttonOperation');
+  } else if (props.value === '=') {
+    customStyle.push('buttonRed');
+  }
+  return (
+    <li style={props.value === '=' ? { width: '44%' } : { width: '20%' }}>
+      <button
+        className={customStyle.join('')}
+        type="button"
+        onClick={props.clicked}
+        value={props.value}
+      >
+        {props.value}
+      </button>
+    </li>
+  );
+};
 
 export default Button;
