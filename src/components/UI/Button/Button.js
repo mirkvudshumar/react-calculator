427726acf60aac558e8438673fb8866c2e8b4373
segasunset95 <nikolovskialexxandar@gmail.com>
2021-03-18 18:28:48 +0100

    Remove unnecessary styles, add M button style

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,17 +3,20 @@ import React from 'react';
 import './Button.css';
 
 const Button = (props) => {
-  const classes = ['button'];
+  const classes = [];
   if (props.buttonStyle) {
     classes.push(props.buttonStyle);
   }
 
   return (
-    <div className={classes.join(' ')}>
-      <button type="button" onClick={props.clicked} value={props.value}>
-        {props.value}
-      </button>
-    </div>
+    <button
+      className={classes.join(' ')}
+      type="button"
+      onClick={props.clicked}
+      value={props.value}
+    >
+      {props.value}
+    </button>
   );
 };
 
